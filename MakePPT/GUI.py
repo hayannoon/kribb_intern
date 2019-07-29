fe2c3dafbc69df98bf1716aafb44530815dd34da
hayannoon95 <hayannoon95@gmail.com>
2019-07-29 13:01:19 +0900

    add radiobox

--- MakePPT/GUI.py
+++ MakePPT/GUI.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from pptx import Presentation
-from PyQt5.QtWidgets import (QApplication, QComboBox, QVBoxLayout,QWidget, QPushButton, QDesktopWidget,QGridLayout, QLabel, QLineEdit, QTextEdit)
+from PyQt5.QtWidgets import (QApplication, QComboBox, QVBoxLayout,QWidget, QPushButton, QDesktopWidget,QGridLayout, QLabel, QLineEdit,QRadioButton)
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QCoreApplication
 import os
@@ -12,14 +12,17 @@ import makePPT_V2
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
-words_array = ['창세기','출애굽기','레위기','민수기','신명기','여호수아']
+old_array = ['창세기','출애굽기','레위기','민수기','신명기','여호수아','사사기','룻기','사무엘상','사무엘하','열왕기상','열왕기하','역대상','역대하','에스라','느혜미야',
+               '에스더','욥기','시편','잠언','전도서','아가','이사야','예레미야','예레미야애가','에스겔','다니엘','호세아','요엘','아모스','오바댜','요나','미가','나훔',
+               '하박국','스바냐','학개','스가랴','말라기']
+new_array = ['마태복음','마가복음','누가복음','요한복음','사도행전','로마서','고린도전서','고린도후서','갈라디아서','에베소서','빌립보서','골로새서','데살로니가전서','데살로니가후서',
+             '디모데전서','디모데후서','디도서','빌레몬서','히브리서','야고보서','베드로전서','베드로후서','요한일서','요한이서','요한삼서','유다서','요한계시록']
 cb_value = ""
 start_value = ""
 chapter_value = ""
 end_value = ""
 title_value = ""
 class MyApp(QWidget):
-
 
     def __init__(self):
         super().__init__()
@@ -31,12 +34,17 @@ class MyApp(QWidget):
         self.lbl = QLabel('',self)
         self.lbl.move(0,0)
         cb = QComboBox(self)
-        cb.addItems(words_array)
+        cb.addItems(old_array)
 
         chapter = QLineEdit(self)
         start = QLineEdit(self)
         end = QLineEdit(self)
         title = QLineEdit(self)
+        old = QRadioButton('구약', self)
+        new = QRadioButton('신약', self)
+
+
+        old.setChecked(True)
 
         grid = QGridLayout()
         self.setLayout(grid)
@@ -47,7 +55,10 @@ class MyApp(QWidget):
         grid.addWidget(QLabel('끝 절:'), 3, 0)
         grid.addWidget(QLabel('제목:'), 4, 0)
 
-
+        old.move(60,20)
+        new.move(120,20)
+        #grid.addWidget(old,0,1)
+        #grid.addWidget(new,0,2)
         grid.addWidget(cb, 0, 1)
         grid.addWidget(chapter, 1, 1)
         grid.addWidget(start, 2, 1)
@@ -72,7 +83,23 @@ class MyApp(QWidget):
 
         btn.clicked.connect(lambda: self.make(str(title.text()),str(cb.currentText()),str(chapter.text()),str(start.text()),str(end.text()),))
 
+        old.clicked.connect(lambda: self.radioButtonClickedOld(cb))
+        #old.clicked.connect(self.radioButtonClickedOld)
+        new.clicked.connect(lambda: self.radioButtonClickedNew(cb))
+
+        print('open')
         self.show() #창을 띄운다.
+
+
+    def radioButtonClickedOld(self,combobox):
+        combobox.clear()
+        combobox.addItems(old_array)
+
+    def radioButtonClickedNew(self,combobox):
+        combobox.clear()
+        combobox.addItems(new_array)
+
+
 
     def onActivated(self, text):
         self.lbl.setText(text)
@@ -125,8 +152,8 @@ class MyApp(QWidget):
 
 
         try: #디렉터리 생성
-            if not(os.path.isdir(Chapter)):
-                os.makedirs(os.path.join(Chapter))
+            if not(os.path.isdir('output folder')):
+                os.makedirs(os.path.join('output folder'))
         except OSError as e:
             if e.errno != errno.EEXIST:
                 print("Failed to create directory!!!!!")
@@ -157,4 +184,5 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv) #어플리케이션 객체 생성
     ex = MyApp()
+    ex.show()
     sys.exit(app.exec_())
